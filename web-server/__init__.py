70d670ad215d2dc14ea5e1eee3dd70bccd3326a7
Ryan Cohen <rcohenprogramming@gmail.com>
2021-05-19 18:51:17 -0400

    Simple index and inputs

--- web-server/__init__.py
+++ web-server/__init__.py
@@ -1,7 +1,13 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route("/")
 def index_page():
     return render_template('index.html')
+
+@app.route("/data", methods=['GET', 'POST'])
+def data_page():
+    if request.method == 'POST':
+        print(f"{request.form['data1']} {request.form['data2']}")
+    return render_template('data_input.html')
