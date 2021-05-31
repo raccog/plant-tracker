3462f679304a9504066bfe1f3f2f31669fe093d0
Ryan Cohen <rcohenprogramming@gmail.com>
2021-05-31 13:39:39 -0400

    Created a function for posting a record to the current table.

---
+++ web-server/static/js/database.js
@@ -0,0 +1,15 @@
+function post_record(record) {
+    if (record.length != 8) return;
+
+    var xhr = new XMLHttpRequest();
+    var url = "post/table_record";
+    xhr.open("POST", url);
+
+    xhr.setRequestHeader
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState == 4) {
+            console.log(xhr.responseText);
+        }
+    }
+    xhr.send(JSON.stringify(record));
+}
