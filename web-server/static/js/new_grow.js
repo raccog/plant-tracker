d656c5c52ace20db699cc53690297eabd536b035
Ryan Cohen <rcohenprogramming@gmail.com>
2021-06-18 18:05:00 -0400

    Added confirmation box when submitting a new grow.

--- web-server/static/js/new_grow.js
+++ web-server/static/js/new_grow.js
@@ -1,13 +1,25 @@
 var p0 = document.getElementById("p0");
 var p1 = document.getElementById("p1");
+var msg = document.getElementById("msg");
+
+function postPlants(plants) {
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", "/post/new_grow", true);
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    xhr.send(JSON.stringify(plants));
+}
 
 function onSubmit() {
     var t0 = p0.value;
     var t1 = p1.value;
     console.assert(t0 != "" && t1 != "");
-
-    var xhr = new XMLHttpRequest();
-    xhr.open("POST", "/post/new_grow", true);
-    xhr.setRequestHeader('Content-Type', 'application/json');
-    xhr.send(JSON.stringify([t0, t1]));
+    if (confirm('Are you sure you want to change to a new grow with plants: '
+        + t0 + ' and ' + t1 + '? This cannot be undone in a GUI yet.')) {
+        postPlants([t0, t1]);
+        msg.style.color = 'green';
+        msg.textContent = 'New grow was set';
+    } else {
+        msg.style.color = 'red';
+        msg.textContent = 'Process to set a new grow was cancelled.';
+    }
 }
