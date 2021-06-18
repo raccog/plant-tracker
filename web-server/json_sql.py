a164f965fb92d56e12a27d60298a144609304e48
Ryan Cohen <rcohenprogramming@gmail.com>
2021-06-18 18:15:44 -0400

    Fixed a wrong path.

--- web-server/json_sql.py
+++ web-server/json_sql.py
@@ -1,9 +1,11 @@
 import json
 import sqlite3
+import os.path as path
 
 
 def pull_nutrients():
-    con = sqlite3.connect('data/NUTRIENT-SCHEDULE.db')
+    from .settings import settings
+    con = sqlite3.connect(path.join(settings.db_path, 'NUTRIENT-SCHEDULE.db'))
     cur = con.cursor()
 
     return json.dumps([{row[0]: list(row[1:]) for row in cur.execute("select * from schedule")}])
