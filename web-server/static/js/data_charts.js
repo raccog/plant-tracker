406624e308fca38408b885d67f8c5a4521fcfcc5
Ryan Cohen <rcohenprogramming@gmail.com>
2021-06-30 14:02:25 -0400

    Added nutrient and supplement data in mL per gallon to the data charts.

--- web-server/static/js/data_charts.js
+++ web-server/static/js/data_charts.js
@@ -11,7 +11,11 @@ let selected_plant = inpPlant.value;
 // Callbacks
 function updatePlant(value) {
     selected_plant = value;
-    replaceChart(selected_plant);
+    if (nutrient_data[selected_plant] == null) {
+        getNutrientData(selected_plant, _ => replaceChart(selected_plant));
+    } else {
+        replaceChart(selected_plant);
+    }
 }
 
 function replaceChart(id) {
@@ -20,19 +24,98 @@ function replaceChart(id) {
     for (let i = 0; i < labels.length; ++i) {
         labels[i] = new Date(parseFloat(labels[i]) * 1000);
     }
-    const microdata = [];
+    const microdata = {
+        x: labels,
+        y: [],
+        name: 'FloraMicro',
+        mode: 'lines+markers',
+        line: {
+            dash: 'solid',
+            color: 'rgb(10, 10, 10)',
+            width: 4
+        }
+    };
+    const vegdata = {
+        x: labels,
+        y: [],
+        name: 'FloraVeg',
+        mode: 'lines+markers',
+        line: {
+            dash: 'dashdot',
+            color: 'rgb(43, 255, 0)',
+            width: 3
+        }
+    };
+    const bloomdata = {
+        x: labels,
+        y: [],
+        name: 'FloraBloom',
+        mode: 'lines+markers',
+        line: {
+            dash: 'dash',
+            color: 'rgb(255, 25, 25)',
+            width: 3
+        }
+    };
+    const calmagdata = {
+        x: labels,
+        y: [],
+        name: 'CalMag',
+        mode: 'lines+markers',
+        line: {
+            dash: 'solid',
+            color: 'rgb(0, 255, 255)',
+            width: 3
+        }
+    };
+    const phupdata = {
+        x: labels,
+        y: [],
+        name: 'pH Up',
+        mode: 'lines+markers',
+        line: {
+            dash: 'dash',
+            color: 'rgb(0, 0, 255)',
+            width: 2
+        }
+    };
+    const phdowndata = {
+        x: labels,
+        y: [],
+        name: 'pH Down',
+        mode: 'lines+markers',
+        line: {
+            dash: 'dash',
+            color: 'rgb(255, 128, 0)',
+            width: 2
+        }
+    };
     for (let key in nutrient_data[id]) {
         const week = nutrient_data[id][key][3];
         const gals = nutrient_data[id][key][0];
         const percent = nutrient_data[id][key][2] * 0.01;
-        microdata.push(nutrient_schedule[week][0] * gals * percent);
+        microdata['y'].push(nutrient_schedule[week][0] * percent);
+        vegdata['y'].push(nutrient_schedule[week][1] * percent);
+        bloomdata['y'].push(nutrient_schedule[week][2] * percent);
+        if (nutrient_data[id][key][6] == 1) {
+            calmagdata['y'].push(nutrient_schedule[week][3] * percent);
+        } else {
+            calmagdata['y'].push(0);
+        }
+        phupdata['y'].push(nutrient_data[id][key][4] / gals);
+        phdowndata['y'].push(nutrient_data[id][key][5] / gals);
     }
-    const data = [{
-        x: labels,
-        y: microdata,
-        type: 'scatter',
-    }];
-    Plotly.newPlot(mychart, data);
+    const data = [microdata, vegdata, bloomdata, calmagdata, phupdata, phdowndata];
+    const layout = {
+      title: 'mL per Gallon of Nutrients and Supplements Over Time',
+      xaxis: {
+        title: 'Date'
+      },
+      yaxis: {
+        title: 'mL per Gallon'
+      }
+    };
+    Plotly.newPlot(mychart, data, layout);
 }
 
 // GET requests
